3fc7079acae016b1e32a4f595be4ffb3c07bf4ab
timtimtim.eth <timothyshen1@hotmail.com>
2026-03-23 15:35:49 +0800

    Initial Commit

---
+++ haiguitang/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  haiguitang
+//
+//  Created by timtimtim on 23/03/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
